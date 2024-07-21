4008117d04c441632501d133fbdce27be7ad314a
Aaminamaxamed <aaminamaxamed630@gmail.com>
2024-07-21 17:41:58 +0300

    Add files via upload

---
+++ src/app/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";   
+import ContactUs from '../contact/index'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <ContactUs />
+   
+  );
+}
---
+++ src/content_option.js
@@ -0,0 +1,82 @@
+const logotext = 'Amina';
+const meta = {
+  title: 'Amina Mohamed',
+  description:
+    'I’m Amina data scientist _ Full stack devloper,currently working in Berlin',
+};
+
+const introdata = {
+  title: 'I’m amina',
+  animated: {
+    first: 'I love coding',
+    second: 'I code cool websites',
+    third: 'I develop mgrahic designer',
+  },
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum',
+  your_img_url: 'https://images.unsplash.com/photo-1514790193030-c89d266d5a9d',
+};
+
+const dataabout = {
+  title: 'abit about my self',
+  aboutme:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.',
+};
+const worktimeline = [
+  {
+    jobtitle: 'Designer ',
+    where: 'xamarjajab',
+    date: '2020',
+  },
+  {
+    jobtitle: 'Designer ',
+    where: 'hargeysa',
+    date: '2021',
+  },
+  {
+    jobtitle: 'Designer',
+    where: 'ALquds',
+    date: '2022',
+  },
+];
+
+
+
+const services = [
+  {
+    title: 'UI & UX Design',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+  {
+    title: 'graphic designer',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+  {
+    title: 'Wordpress Design',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.',
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: 'amuunamaxamed23@gmail.com',
+  YOUR_FONE: '(2526167507977',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
+  YOUR_SERVICE_ID: 'service_id',
+  YOUR_TEMPLATE_ID: 'template_id',
+  YOUR_USER_ID: 'user_id',
+};
+
+ 
+export {
+  meta,
+  dataabout,
+  worktimeline,
+  services,
+  introdata,
+  contactConfig, 
+  logotext,
+};
